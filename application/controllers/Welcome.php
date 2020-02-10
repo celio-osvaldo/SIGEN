2ed13572b6d80b775375d3bed03abf23948ec229
Aramis de Jesus Garnica Oropeza <aramis.garnica@softtek.com>
2020-02-10 00:12:45 -0600

    Se agrego el archivo .htaccess; Se modifico el archivo atoload se agrego la librería session, se modifico el archivo config la base_url estaba mal escrita; Se agrego un nueevo controlador llamado DASA; Se modificó el archivo del controlador Welcome agregando una funcion para redireccionamiento a otro controlador, se modifico el archivo in.php linea 34,35 y 40

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,27 +3,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
 	public function index()
 	{
-		#return view('welcome_message');
-	   $data['title']='DASASYS';
-	   $this->load->view('plantillas/header', $data);
-	   $this->load->view('Log/in');
-       $this->load->view('plantillas/footer');
+		$data['error'] = $this->session->flashdata('error');
+		$data['title']='DASASYS';
+		$this->load->view('Log/in', $data);
 	}
+
+	public function LogDasa(){
+		redirect('/Dasa/Index', 'refresh');
+	}
+
 }

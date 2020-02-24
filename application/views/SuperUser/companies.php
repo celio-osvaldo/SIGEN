5de0ff3b822be257fd34f3af1772dba8108a1401
Aramis-Garnica <aramisgarnica@gmail.com>
2020-02-24 16:53:24 -0600

    Modificación: se modifico el archivo Controller>welcome.php, se removio la funcion de companies() para separar las vistas; Se agregó en Controllers>SuperUsuario.php controlador para las acciones del super usuario, Modificación: Se agregó la carpeta para las vistas de super usuarios.

---
+++ application/views/SuperUser/companies.php
@@ -0,0 +1,32 @@
+<div class="jumbotron jumbotron-fluid">
+    <br>
+
+      <div class=" col-lg-12 card bg-card">
+      <br>        
+        <div class="row">
+          <div class="col-sm-1"></div>
+          <div class="card text-center" style="width: 15rem;">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <a href="#"><img class="img-fluid" src="<?php echo base_url() ?>Resources/Logos/DASA_logo.png"></a>
+          </div>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="card text-center" style="width: 15rem;">
+          <div class="card-header"></div>
+          <div class="card-body">
+            <a href="#"><img class="img-fluid" src="<?php echo base_url() ?>Resources/Logos/Logo_ISA.png"></a> 
+          </div>
+        </div>
+        <div class="col-md-1"></div>
+        <div class="card text-center" style="width: 15rem;">
+          <div class="card-header"></div>
+          <div class="card-body">
+            
+          <a href="#"><img class="img-fluid" src="<?php echo base_url() ?>Resources/Logos/SALINAS.png"></a>
+          </div>
+        </div>
+        </div> 
+        <br>
+      </div>
+</div>

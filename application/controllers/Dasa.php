62426acfa80708ab4c906fec53f26cc929e675e8
Aramis-Garnica <aramisgarnica@gmail.com>
2020-02-26 13:05:49 -0600

    Modificación: GeneralStyles.css se cambiaron las propiedades de lso headers debdido a que causaban conflictos con las vistas sobre poninedo los objetos en el navbar.

--- application/controllers/Dasa.php
+++ application/controllers/Dasa.php
@@ -29,5 +29,21 @@ class DASA extends CI_Controller {
 		$this->LogIn();
 	}
 
+	public function GetInventories(){
+		if ($this->session->userdata('usuario_alias')) {#verified if a user is logged and don´t lose the session
+          $data['alias'] = $this->session->userdata('usuario_alias');#Return the name alias of user for showing
+          $data['type'] = $this->session->userdata('nombre_tipo');#it will know who type of user start session and show its navbar
+          $data['corp'] = $this->session->userdata('empresa_nom');#for applicated the color in navbar
+			$data['title']='SiGeN | DASA';
+	   		$this->load->view('plantillas/header_dasa', $data);
+			$this->load->view('DASA/InventoriesList');
+       		$this->load->view('plantillas/footer_dasa');
+       	}
+       	else{
+       		$this->session->set_flashdata('error', 'No ha iniciado Sesión');//if not exist the user, just show an error in the view
+       		redirect('/');
+       	}
+	}
+
 }
  
---
+++ application/views/DASA/InventoriesList.php
@@ -0,0 +1,33 @@
+<br>
+
+<div class="row">
+    
+    <div class="col-md-1"></div>
+    <div class="col-md-10">
+        <div class="card bg-card">
+            <div class="container">
+                <table id="table_id" class="display">
+                    <thead>
+                        <tr>
+                            <th>Column 1</th>
+                            <th>Column 2</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Row 1 Data 1</td>
+                            <td>Row 1 Data 2</td>
+                        </tr>
+                        <tr>
+                            <td>Row 2 Data 1</td>
+                            <td>Row 2 Data 2</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-1"></div>
+</div>
+
+<br>

2c30ab6b5d21ccfe9bc4f9b9ec41a954c4581e39
celio-osvaldo <celio_osvaldo_02@hotmail.com>
2020-02-27 22:45:07 -0600

    Se agrega nueva vista para Clientes/Obras y carpeta de Jquery

---
+++ application/views/DASA/Customer_Projects.php
@@ -0,0 +1,10 @@
+
+<div class="container-md">
+    <div class="form-group col-md-2">
+      <label for="search">Nombre Obra/Cliente</label>
+      <input type="search" class="form-control" id="search">
+    </div>
+    <?php 
+        echo rand(1,1000);
+     ?>
+</div>

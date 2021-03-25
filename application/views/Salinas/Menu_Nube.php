20fb726c97e75c03afc3f8003c99d95053936650
Celio Osvaldo Cruz Mora <60762220+celio-osvaldo@users.noreply.github.com>
2021-03-24 21:48:45 -0600

    Corrección Tabla Menu Nube
    Corrección para evitar que el auto completado del navegador ingrese datos en el campo de búsqueda

--- application/views/Salinas/Menu_Nube.php
+++ application/views/Salinas/Menu_Nube.php
@@ -399,7 +399,26 @@
 
 <script type="text/javascript">
 	$(document).ready(function(){
-		$('#table_nube').DataTable();
+		$('#table_nube').DataTable({
+        initComplete: function() {
+            $(this.api().table().container()).find('input').parent().wrap('<form>').parent().attr('autocomplete', 'off');
+        },
+         /****** add this */
+        "searching": true,
+        // "autoFill": true,
+        "language": {
+            "lengthMenu": "Por página: _MENU_",
+            "zeroRecords": "Sin resultados",
+            "info": "Mostrando página _PAGE_ de _PAGES_",
+            "infoEmpty": "No hay registros disponibles",
+            "infoFiltered": "(Filtrado de _MAX_ registros en total)",
+            "search": "Búsqueda",
+                "paginate": {
+			      "previous": "Anterior",
+			      "next": "Siguiente"
+			    }
+        },
+		});
 		
 		$('#crearcarpeta').click(function(){
 			ruta_carpeta=$("#ruta_nueva_carpeta").val();
